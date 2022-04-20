9615d97ff4f51d7c1a559077d1aaa61a43f931ca
Anna Lezhikova <anna.v.l@gmail.com>
2022-04-20 20:11:30 +1200

    use random voice

--- utils.py
+++ utils.py
@@ -1,30 +1,26 @@
+import random
+
 def align_times(structure, voices):
     result = [[] for _ in range(len(structure))]
     for current_beat, form in enumerate(structure):
-        print("start beat")
         n_voices = form[0]
-        voice_number = 0
+        voices_order = random.sample(range(3), 3)
+        voice_index = 0
         for _ in voices:
-            print("start voice", len(result[current_beat]), n_voices, len(voices))
             if len(result[current_beat]) < len(voices):
-                if len(result[current_beat]) < n_voices:
-                    current_voice = voice_number + 1
+                if len(result[current_beat]) < n_voices and len(voices[voices_order[voice_index]]) > 0:
+                    current_voice = voices_order[voice_index] + 1
                     if not current_voice in result[current_beat]:
-                        print("New voice")
-                        cell_length = voices[voice_number][0][1]
+                        cell_length = voices[voices_order[voice_index]][0][1]
                         relative_beat = current_beat
                         while cell_length > 0:
-                            print("REL BEAT", relative_beat, len(structure))
                             if relative_beat < len(structure):
-                                result[relative_beat].append(voice_number + 1)
+                                result[relative_beat].append(voices_order[voice_index] + 1)
                             cell_length -= 1
                             relative_beat += 1
-                        del voices[voice_number][0]
+                        del voices[voices_order[voice_index]][0]
                 else:
                     result[current_beat].append(0)
-                voice_number += 1
-            print("end voice", result)
-        print("end beat",result)
-    print(result)
+                voice_index += 1
     return result
 
