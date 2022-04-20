7be2ed58da9446b2291d0b4d9a4829ab8339584c
Anna Lezhikova <anna.v.l@gmail.com>
2022-04-20 19:50:06 +1200

    works with same voices order

--- test_utils.py
+++ test_utils.py
@@ -1,9 +1,9 @@
 from utils import align_times
 
 def test_align_times():
-    structure = [[2,1], [1,5], [3,6]]
-    voices = [[["1-1",1],["1-2",2]],[["2-1",1],["2-2",1]],[["3-1",1],["3-2",3]]]
-    expected_result = [[1,2,0],[1,0,0],[1,2,3]]
+    structure = [[2,1], [1,5], [3,6], [1,3]]
+    voices = [[["1-1",1],["1-2",2]],[["2-1",1],["2-2",1]],[["3-1",3],["3-2",1]]]
+    expected_result = [[1,2,0],[1,0,0],[1,2,3],[3,0,0]]
     result = align_times(structure, voices) 
     assert len(result) == len(expected_result)
     for i in result:
--- utils.py
+++ utils.py
@@ -1,26 +1,30 @@
-from operator import contains
-
-
 def align_times(structure, voices):
-    result = [[] for i in range(len(structure))]
+    result = [[] for _ in range(len(structure))]
     for current_beat, form in enumerate(structure):
+        print("start beat")
         n_voices = form[0]
         voice_number = 0
-        for i in voices:
-            if len(result[current_beat]) < n_voices:
-                current_voice = voice_number + 1
-                if not current_voice in result[current_beat]:
-                    cell_length = voices[voice_number][0][1]
-                    relative_beat = current_beat
-                    while cell_length > 0:
-                        result[relative_beat].append(voice_number + 1)
-                        cell_length -= 1
-                        relative_beat += 1
-                    del voices[voice_number][0]
-            else:
-                result[current_beat].append(0)
-            n_voices -= 1
-            voice_number += 1
+        for _ in voices:
+            print("start voice", len(result[current_beat]), n_voices, len(voices))
+            if len(result[current_beat]) < len(voices):
+                if len(result[current_beat]) < n_voices:
+                    current_voice = voice_number + 1
+                    if not current_voice in result[current_beat]:
+                        print("New voice")
+                        cell_length = voices[voice_number][0][1]
+                        relative_beat = current_beat
+                        while cell_length > 0:
+                            print("REL BEAT", relative_beat, len(structure))
+                            if relative_beat < len(structure):
+                                result[relative_beat].append(voice_number + 1)
+                            cell_length -= 1
+                            relative_beat += 1
+                        del voices[voice_number][0]
+                else:
+                    result[current_beat].append(0)
+                voice_number += 1
+            print("end voice", result)
+        print("end beat",result)
     print(result)
     return result
 
